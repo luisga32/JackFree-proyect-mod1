8cc002e240deeafb93532f7463873b25b199f452
luisga32 <luisga32@hotmail.com>
2021-01-06 13:36:18 +0100

    improve movements player

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,7 +6,7 @@ class Game {
     this.fps = 1000 / 60;
     this.board = new Board(this.ctx);
     this.player = new Player(this.ctx, POSX_INI_PLAYER, POSY_INI_PLAYER)
- 
+
     this.objects = []
     this.ghosts = []
     this.numMaxGhosts = 5
@@ -22,14 +22,14 @@ class Game {
     this.newGhost = undefined;
     this.ghostimgWidth = 33.64;
 
-   this.theme = new Audio('./assets/sound/ghost02.mp3')
+    this.theme = new Audio('./assets/sound/ghost02.mp3')
     this.theme.volume = 0.2
     this.captureSound = new Audio('./assets/sound/shooting_star-Mike_Koenig-1132888100.mp3')
-    this.captureSound.volume= 0.2
-    this.state ={
-      new:true,
-      pause:false,
-      ended:false
+    this.captureSound.volume = 0.2
+    this.state = {
+      new: true,
+      pause: false,
+      ended: false
 
     }
 
@@ -41,66 +41,25 @@ class Game {
 
 
     if (!this.drawInterval) {
-      
+
       this.drawInterval = setInterval(() => {
         //console.log('start game')
-        // control de pulsaciones del teclado
-        //        this.setListeners()
-        // limpio canvas
-        //       this.clear()
-        //muevo objetos
-        //      this.move()
-        // check collitions betwwen car and obstacles
-
-        // dibujo objetos
+ 
+  
         this.theme.play();
         this.clear();
         this.move();
         this.draw();
-
-
-
-        if (this.numghosts < this.numMaxGhosts) {
-          this.addGhost();
-
-        }
-        else if (!this.addGhostTimeoutID){
-           
-          this.addGhostTimeoutID = setTimeout(() => {
-            this.numghosts = 0;
-            this.ghostY = 0;
-            this.newGroupGhosts = true;
-            this.addGhostTimeoutID =undefined;
-          }
-            , GHOST_FRAME)
-        }
-        
-
-
-
-        this.objCountDraw++
-        if (this.objCountDraw % OBJECTS_FRAME === 0 && this.objects.length <= this.numMaxobjects) {
-          if (this.objects.length < this.numMaxobjects) {
-          this.addObject();
-        } else {
-          this.removeObject();
-        }
-          this.objCountDraw = 0
-        }
-
-
-        //      this.obstDrawCount++
-        // add obstacles  
-        //      if (this.obstDrawCount % OBSTACLES_FRAMES === 0) {
-        //           this.addObstacle()
-
-        //          this.obstDrawCount = 0
-        //        }
         //check collitions
         this.checkCollisions();
- 
-        //}
-        
+        // add Ghost
+        this.validateAddGhost();
+        // add object to capture
+        this.validateAddObject(); 
+
+
+
+
       }, this.fps)
     }
   }
@@ -117,19 +76,19 @@ class Game {
   draw() {
     this.board.draw();
     this.drawScore();
-
-    if (this.player.isPlayerDead()){
-      // if (this.player.playerDead.isReady())
-   
-       this.player.drawDead();
-      
-       this.pause()
-    } else {
-    this.player.draw();
-
-  }
     this.objects.forEach(object => object.draw());
     this.ghosts.forEach(ghost => ghost.draw());
+    if (this.player.isPlayerDead()) {
+  
+
+      this.player.drawDead();
+
+      this.pause()
+    } else {
+      this.player.draw();
+
+    }
+
 
 
 
@@ -144,6 +103,18 @@ class Game {
     this.player.move();
     this.ghosts.forEach(ghost => ghost.move())
   }
+  validateAddObject() {
+    this.objCountDraw++
+    if (this.objCountDraw % OBJECTS_FRAME === 0 && this.objects.length <= this.numMaxobjects) {
+      if (this.objects.length < this.numMaxobjects) {
+        this.addObject();
+      } else {
+        this.removeObject();
+      }
+      this.objCountDraw = 0
+    }
+
+  }
 
   addObject() {
     // select one object ramdomly from IMAGES array
@@ -151,50 +122,58 @@ class Game {
     // create a new object
 
     const newObject = new Object(this.ctx, IMAGES[index]);
- 
-  
+
+
     // checks if there is some object in the coordenates calculates for the new object. In that case create a new object
-    const objIntervalId = setInterval (() => {
-      if (newObject.isReady()){
+    const objIntervalId = setInterval(() => {
+      if (newObject.isReady()) {
         if (this.objects.some(object => newObject.collideWith(object))) {
-         this.addObject();
+          this.addObject();
         } else {
-         this.objects.push(newObject);
-        
-       }
+          this.objects.push(newObject);
 
-       clearInterval(objIntervalId);
-     }
-   })
-   // checks if there is some object in the coordenates calculates for the new object. In that case recalculate the coordenates
- //  while (this.objects.some(object => newObject.collideWith(object))) {
- //    newObject = new Object(this.ctx, IMAGES[index]);
- //  }
+        }
 
-
-    //add object to objects array
-  //  this.objects.push(newObject);
+        clearInterval(objIntervalId);
+      }
+    })
+ 
 
   }
   removeObject() {
     this.objects.shift();
   }
 
-  
-  
+  validateAddGhost() {
+    if (this.numghosts < this.numMaxGhosts) {
+      this.addGhost();
+
+    }
+    else if (!this.addGhostTimeoutID) {
+
+      this.addGhostTimeoutID = setTimeout(() => {
+        this.numghosts = 0;
+        this.ghostY = 0;
+        this.newGroupGhosts = true;
+        this.addGhostTimeoutID = undefined;
+      }
+        , GHOST_FRAME)
+    }
+  }
+
 
   addGhost() {
 
     if (this.newGroupGhosts) {
-      this.ghostX =Math.floor(Math.random() * (this.width - this.ghostimgWidth))
+      this.ghostX = Math.floor(Math.random() * (this.width - this.ghostimgWidth))
       this.newGhost = new Ghost(this.ctx, this.ghostX, this.ghostY);
-      if (!this.ghosts.some(ghost => this.newGhost.collidesWith(ghost))){
+      if (!this.ghosts.some(ghost => this.newGhost.collidesWith(ghost))) {
         this.newGroupGhosts = false;
       }
     } else {
       if (this.newGhost.isReady()) {
 
-     
+
         this.ghosts.unshift(this.newGhost);
         this.numghosts++;
         if (this.numghosts < this.numMaxGhosts) {
@@ -209,82 +188,82 @@ class Game {
 
 
 
-  clearGhosts(){
-    this.ghosts = this.ghosts.filter(ghost => ghost.y  <= this.height);
-   }
-  
+  clearGhosts() {
+    this.ghosts = this.ghosts.filter(ghost => ghost.y <= this.height);
+  }
+
   // check collitions between playes and the others elements of the screen 
   checkCollisions() {
     // collitions with ghosts
     const ghostCollided = this.ghosts.some(ghost => this.player.collidesWith(ghost));
 
-    if (ghostCollided){
-              this.state.ended = true
-              this.player.CreateplayerDead();   
- 
+    if (ghostCollided) {
+      this.state.ended = true
+      this.player.CreateplayerDead();
+
     }
-      // collition with and object 
-      const objCollided = this.objects.filter(object => this.player.collidesWith(object));
-      let index;
-      if (objCollided.length > 0) {
-        this.captureSound.play();
-        for (let i = 0; i < objCollided.length; i++) {
-  
-          this.points += objCollided[i].object.points;
-          //    console.log(this.points) 
-        
-          objCollided[i].pointsDraw();
-          index = this.objects.indexOf(objCollided[i]);
-          this.objects.splice(index, 1);
-        }
-  
-  
+    // collition with and object 
+    const objCollided = this.objects.filter(object => this.player.collidesWith(object));
+    let index;
+    if (objCollided.length > 0) {
+      this.captureSound.play();
+      for (let i = 0; i < objCollided.length; i++) {
+
+        this.points += objCollided[i].object.points;
+        //    console.log(this.points) 
+
+        objCollided[i].pointsDraw();
+        index = this.objects.indexOf(objCollided[i]);
+        this.objects.splice(index, 1);
       }
 
+
+    }
+
   }
-  pause(){
-    
-    this.state.pause =true;
+  pause() {
+
+    this.state.pause = true;
     const startButton = document.getElementById("start-button");
     startButton.disabled = false;
     this.theme.pause();
 
     clearInterval(this.drawInterval)
-  
-  }
-  initialice(){
-    const startButton = document.getElementById("start-button");
-    startButton.disabled = true;  
 
-    if (this.state.ended){
+  }
+  initialice() {
+    const startButton = document.getElementById("start-button");
+    startButton.disabled = true;
+
+    if (this.state.ended) {
       this.theme.load();
 
-    this.objects = []
-    this.ghosts = []
-    this.numMaxGhosts = 5
-    this.addGhostTimeoutID = undefined;
-   
-    this.objCountDraw = 0
-    this.numMaxobjects = 5;
-    this.points = 0
-    this.numghosts = 0
-    this.ghostY = 0;
-    this.ghostX = 0;
-    this.newGroupGhosts = true;
-    this.newGhost = undefined;
-    
-    this.player.initialice();
-  }
- 
-  
-  this.drawInterval = undefined;
-  this.state.pause =false;
-  this.state.ended=false;
+      this.objects = []
+      this.ghosts = []
+      this.numMaxGhosts = 5
+      this.addGhostTimeoutID = undefined;
+
+      this.objCountDraw = 0
+      this.numMaxobjects = 5;
+      this.points = 0
+      this.numghosts = 0
+      this.ghostY = 0;
+      this.ghostX = 0;
+      this.newGroupGhosts = true;
+      this.newGhost = undefined;
+
+      this.player.initialice();
+    }
+
+
+    this.drawInterval = undefined;
+    this.state.pause = false;
+    this.state.ended = false;
 
   }
 
-  drawScore (){
-     document.querySelector('#score').innerHTML = 'SCORE: ' + this.points
+  drawScore() {
+    document.querySelector('#score').innerHTML = 'SCORE: ' + this.points
 
   }
 
--- assets/js/player.js
+++ assets/js/player.js
@@ -10,13 +10,14 @@ class Player {
         this.height = 0
 
         this.player = new Image();
-        this.player.src = "./assets/img/jackfree/spritesheet.png";
+  
+        this.player.src = "./assets/img/jackfree/spritesheet3.png";
         this.playerDead = undefined;
         this.player.isReady = false;
         this.isplayerDead = false;
         this.playerDeadIsReady = false
         this.player.horizontalFrames = 9
-        this.player.verticalFrames = 1
+        this.player.verticalFrames = 2
         this.player.horizontalFrameIndex = 0
         this.player.verticalFrameIndex = 0
         this.player.scale = 13
@@ -108,19 +109,45 @@ class Player {
     }
 
     resetAnimation() {
+        if (this.lookRight()) {
         this.player.horizontalFrameIndex = 0
         this.player.verticalFrameIndex = 0
+    } else {
+        this.player.horizontalFrameIndex = 8
+        this.player.verticalFrameIndex = 1
+    }
     }
 
 
+/*
+    resetAnimation() {
+  
+        this.player.horizontalFrameIndex = 0
+        this.player.verticalFrameIndex = 0
+  
+    }
 
+    */
     animatePlayer() {
         if (this.player.drawCount % MOVEMENT_FRAMES === 0) {
-            if (this.player.horizontalFrameIndex === this.player.horizontalFrames - 1) {
-                this.player.horizontalFrameIndex = 1;
+            if (this.lookRight()) {
+            if ((this.player.horizontalFrameIndex === this.player.horizontalFrames - 1)
+                || (this.movements.left && this.movements.right)
+                || ( this.movements.up && this.movements.down)) {
+                this.player.horizontalFrameIndex = 0;
             } else {
                 this.player.horizontalFrameIndex++;
             }
+        } else {
+            if (this.player.horizontalFrameIndex === 0
+                || ( this.movements.left && this.movements.right)
+                || ( this.movements.up && this.movements.down)) {
+                this.player.horizontalFrameIndex = this.player.horizontalFrames -1;
+            } else {
+                this.player.horizontalFrameIndex--;
+            }
+
+        }
             this.player.drawCount = 0;
         }
 
@@ -128,50 +155,113 @@ class Player {
 
 
     }
+    /*
+    animatePlayer() {
+        if (this.player.drawCount % MOVEMENT_FRAMES === 0) {
+           
+            if (this.player.horizontalFrameIndex === this.player.horizontalFrames - 1) {
+                this.player.horizontalFrameIndex = 1;
+            } else {
+                this.player.horizontalFrameIndex++;
+            }
+       
+            this.player.drawCount = 0;
+        }
 
+
+
+
+    }
+*/
+    lookRight(){
+        return this.player.verticalFrameIndex === 0
+    }
     onKeyEvent(event) {
         //     console.log (event.type)
         //     console.log(event.keyCode)
-        const status = event.type === 'keydown'
-        switch (event.keyCode) {
-            case KEY_UP:
-                this.movements.up = status
+    
+        switch (event.type) {
+            case 'keydown':
+               
+                switch (event.keyCode) {
+                    case KEY_UP:
+                        this.movements.up = true
+                        break;
+                    case KEY_RIGHT:
+                        this.movements.right = true
+                        break;
+                    case KEY_LEFT:
+        
+                        this.movements.left = true
+                        break;
+                    case KEY_DOWN:
+                        this.movements.down = true
+                        break;
+        
+                    default:
+                        break;
+                }
                 break;
-            case KEY_RIGHT:
-                this.movements.right = status
-                break;
-            case KEY_LEFT:
-
-                this.movements.left = status
-                break;
-            case KEY_DOWN:
-                this.movements.down = status
-                break;
-
+            case 'keyup':
+                switch (event.keyCode) {
+                    case KEY_UP:
+                        this.movements.up = false
+                        break;
+                    case KEY_RIGHT:
+                        this.movements.right = false
+                        break;
+                    case KEY_LEFT:
+        
+                        this.movements.left = false
+                        break;
+                    case KEY_DOWN:
+                        this.movements.down = false
+                        break;
+        
+                    default:
+                        break;
+                }
+                break;                  
             default:
                 break;
         }
+        
+    
+
     }
     move() {
         // si se ha pulsado izquierda cambiamos la coordenada x restando para que se dibuje mas a la izquierda. Si es la derecha sumamos
-
-        if (this.movements.left) {
+    
+        console.log(`ANTES X: ${this.x} Y: ${this.y}`) 
+        this.speedX = 0;
+        this.speedY = 0;
+       
+        if (this.movements.left && !this.movements.right) {
             this.speedX = -SPEED
-        } else if (this.movements.right) {
+   
+            this.player.verticalFrameIndex = 1
+ 
+        } else  if (this.movements.right && !this.movements.left) {
             this.speedX = SPEED;
-        } else if (this.movements.up) {
-            this.speedY = - SPEED;
-        } else if (this.movements.down) {
-            this.speedY = SPEED;
-
+  
+            this.player.verticalFrameIndex = 0
+ 
         }
-        else {
-            this.speedX = 0;
-            this.speedY = 0;
+        
+    
+        if (this.movements.up && !this.movements.down) {
+            this.speedY = - SPEED;
+  
+
+        } else if (this.movements.down && !this.movements.up) {
+            this.speedY = SPEED;
+  
+ 
         }
         // asignamos las nuevas coordenadas al objeto
         this.x += this.speedX;
         this.y += this.speedY;
+ 
         // controlamos límites
 
         if (this.x + this.width >= this.ctx.canvas.width) {
@@ -189,11 +279,7 @@ class Player {
 
     collidesWith(element) {
         // if the four conditions are true there is a collition
-        //      console.log(`player collidewith ${this.x} ${this.y} ${this.image.complete}`)
-
-
-
-
+ 
         return this.x < element.x + element.width  // collition by right side
             && this.x + this.width > element.x              // collition by left side
             && this.y < element.y + element.height   // frontal collition
@@ -205,6 +291,7 @@ class Player {
         this.y = POSY_INI_PLAYER;
         this.player.isplayerDead=false;
         this.playerDeadIsReady = false;
+        this.player.verticalFrameIndex = 0;
     }
     // End Class
 }
